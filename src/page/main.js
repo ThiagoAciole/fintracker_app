1d87048ddec4447570db953f45bbe07fc9b7543a
ThiagoAciole <thiagoaciole7@gmail.com>
2023-11-20 20:49:43 -0300

    commit ajustes

--- src/page/main.js
+++ src/page/main.js
@@ -5,8 +5,9 @@ import Home from "./Home/home";
 import Investiment from "./Investimentos/investiment";
 import Account from "./Account/account";
 import Card from "./Card/card";
+import AppBar from "../components/AppBar/appbar";
 
-export default function Login() {
+export default function Main({ navigation }) {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
     {
@@ -28,10 +29,17 @@ export default function Login() {
   });
 
   return (
-    <BottomNavigation
-      navigationState={{ index, routes }}
-      onIndexChange={setIndex}
-      renderScene={renderScene}
-    />
+    <>
+      {index === 0 ? (
+        <AppBar navigation={() => navigation.navigate("Notification")} />
+      ) : (
+        ""
+      )}
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+      />
+    </>
   );
 }
